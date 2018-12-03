3ebcdacf2e6057a1c2746421300b63864683e0ef
dahai <491753317@qq.com>
2018-12-03 19:34:19 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,7 +5,9 @@
  * Date: 2018/12/3
  * Time: 16:53
  */
-function test_helper()
+
+//此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
+function route_class()
 {
-    return 'OK';
+    return str_replace('.', '-', Route::currentRouteName());
 }
